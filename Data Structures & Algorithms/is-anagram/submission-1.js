0c6ffff0edb440ab0575996f58705acec4e0b1ff
downtownkopi <downtownkopi@gmail.com>
2026-06-02 13:53:23 +0800

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        const sLength = s.length;
+        if (sLength !== t.length) {
+            return false;
+        }
+
+        const sMap = new Map();
+        for (const char of s) sMap.set(char, (sMap.get(char) || 0) + 1);
+
+        for (const char of t) {
+            if (!sMap.has(char)) return false;
+            sMap.set(char, sMap.get(char) - 1);
+            if (sMap.get(char) === 0) sMap.delete(char);
+        }
+
+        return Object.keys(sMap).length === 0;
+    }
+}
