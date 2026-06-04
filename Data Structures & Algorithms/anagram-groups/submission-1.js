dca8556eea7f9460eb47859ee9fef2b655a4f47c
downtownkopi <downtownkopi@gmail.com>
2026-06-04 10:27:37 +0800

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+       const sortedMap = {};
+       for (const str of strs) {
+            const sortedStr = str.split('').sort().join('');
+            if (!sortedMap[sortedStr]) sortedMap[sortedStr] = [];
+            sortedMap[sortedStr].push(str);
+       }
+
+        return Object.values(sortedMap);
+    }
+}
