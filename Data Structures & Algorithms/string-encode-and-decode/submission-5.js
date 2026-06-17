9fb21b0b5a2b705a7fef7f070d708e88361a4832
downtownkopi <downtownkopi@gmail.com>
2026-06-18 00:19:09 +0800

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        const str = strs.join('</><>');
+        return strs.length > 0 ? `<>${strs.join('</><>')}</>` : '';
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        if (str.length === 0) return [];
+
+        const arr = str.split('</><>');
+        arr[0] = arr[0].slice(2);
+        arr[arr.length - 1] = arr[arr.length - 1].slice(0, -3);
+        return arr;
+    }
+}
