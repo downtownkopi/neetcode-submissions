2907159419e789e909f6cd5543a247e7416ce29a
downtownkopi <downtownkopi@gmail.com>
2026-06-30 15:42:11 +0800

    Add: longest-consecutive-sequence - submission-11

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-11.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        if (nums.length === 0) return 0;
+
+        const set = new Set(nums);
+
+        let maxCount = -Infinity;
+        for (const num of set) {
+            if (set.has(num - 1)) continue;
+            
+            let count = 1;
+            let start = num + 1;
+            while (set.has(start)) {
+                count++;
+                start++;
+            }
+            if (count > maxCount) maxCount = count;
+        }
+
+        return maxCount;
+    }
+}
