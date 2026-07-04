d4b3a5fd8a35eed6929d82e5782299372d469eca
downtownkopi <downtownkopi@gmail.com>
2026-07-04 14:42:51 +0800

    Add: trapping-rain-water - submission-8

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-8.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let left = 0, right = height.length - 1;
+        let leftMax = 0, rightMax = 0;
+        let sum = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    sum += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    sum += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return sum;
+    }
+}
