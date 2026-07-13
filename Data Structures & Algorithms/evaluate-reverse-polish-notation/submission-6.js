b43126dce05d07fb8daef5294dc416547f1c0f83
downtownkopi <downtownkopi@gmail.com>
2026-07-13 11:04:25 +0800

    Add: evaluate-reverse-polish-notation - submission-6

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-6.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        const operations = ['+', '-', '*', '/'];
+        const overall = [];
+        const toBeOperated = [null, null];
+
+        const operators = {
+            "+": (a, b) => a + b,
+            "-": (a, b) => a - b,
+            "*": (a, b) => a * b,
+            "/": (a, b) => Math.trunc(a / b),
+        };
+
+        for (let i = 0; i < tokens.length; i++) {
+            if (operations.indexOf(tokens[i]) === -1) {
+                overall.push(tokens[i]);
+                continue;
+            }
+
+            const operation = operators[tokens[i]](Number(overall[overall.length - 2]), Number(overall[overall.length - 1]));
+            overall.pop();
+            overall.pop();
+            overall.push(operation);
+        }
+
+        return overall[0];
+    }
+}
