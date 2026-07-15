4c880a928de254626a214675305021fba23459a7
downtownkopi <downtownkopi@gmail.com>
2026-07-15 09:04:34 +0800

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const waiting = [];
+        const result = new Array(temperatures.length).fill(0);
+        
+        for (let i = 0; i < temperatures.length; i++) {
+            if (waiting.length === 0) {
+                waiting.push(i);
+                continue;
+            } else {
+                while (temperatures[i] > temperatures[waiting[waiting.length - 1]]) {
+                    result[waiting[waiting.length - 1]] = i - waiting[waiting.length - 1];
+                    waiting.pop();
+                }
+
+                waiting.push(i);
+            }
+
+        }
+
+        return result;
+    }
+}
